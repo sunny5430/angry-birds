f29e807b3b836509071a1e2ce6c1ff68970d7dca
Mei-Chi Chen <chiwenwu@mcdb-jbardwell-air.local>
2020-07-21 16:37:09 -0400

    modify bircell to prevent test from hanging

--- AngryBirdsTests/BirdListViewControllerTests.swift
+++ AngryBirdsTests/BirdListViewControllerTests.swift
@@ -14,16 +14,16 @@ class BirdListViewControllerTests: XCTestCase {
 
     override func setUp() {
         super.setUp()
-        
+
         let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
         let navigationController = storyboard.instantiateInitialViewController() as! UINavigationController
         self.systemUnderTest = navigationController.topViewController as? BirdListViewController
-        
+
         UIApplication.shared.windows
             .filter { $0.isKeyWindow }
             .first!
             .rootViewController = self.systemUnderTest
-        
+
         XCTAssertNotNil(navigationController.view)
         XCTAssertNotNil(self.systemUnderTest.view)
     }
@@ -37,24 +37,24 @@ class BirdListViewControllerTests: XCTestCase {
             Bird(named: "Robin", description: "Angry", imageUrl: "birds.com/angryrobin"),
         ]
         mockBirdService.mockBirds = mockBirds
-        
+
         self.systemUnderTest.viewDidLoad()
         self.systemUnderTest.birdService = mockBirdService
-        
+
         XCTAssertEqual(0, self.systemUnderTest.tableView.numberOfRows(inSection: 0))
-        
+
         //When
         self.systemUnderTest.viewWillAppear(false)
-        
+
         //Then
         XCTAssertEqual(mockBirds.count, self.systemUnderTest.flock.count)
         XCTAssertEqual(mockBirds.count, self.systemUnderTest.tableView.numberOfRows(inSection: 0))
     }
-    
+
     class MockBirdService: BirdService {
         var mockBirds: [Bird]?
         var mockError: Error?
-        
+
         override func getBirds(completion: @escaping ([Bird]?, Error?) -> ()) {
             completion(mockBirds, mockError)
         }
